b4920a0e577f60fe979c09edafbf295c112ac90c
Josh <josh.yu_8@live.com>
2017-08-14 19:22:42 +0800

    Added base settings

---
+++ app.py
@@ -0,0 +1,12 @@
+from sanic import Sanic
+from sanic.response import text
+
+
+app = Sanic(__name__)
+
+
+@app.route("/")
+async def test(request):
+    return text('Hello world!')
+
+app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ config.py
@@ -0,0 +1,14 @@
+# Database settings
+
+DATABASE = {
+    'DB_ENGINE': 'sqlite',
+    'DB_NAME': 'mydatabase',
+}
+
+
+# theme settings
+
+THEMES = {
+    'THEME_CMS': 'default',
+    'THEME_ADMIN': 'default',
+}
