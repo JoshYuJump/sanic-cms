fb277203eb3f06f07965c400630a76b03304441f
Josh <josh.yu_8@live.com>
2017-08-14 21:51:54 +0800

    Load config in `App.py`

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import html
+from config import DATABASE, THEMES
 
 
 app = Sanic(__name__)
+app.config.update(DATABASE)
+app.config.update(THEMES)
 
 
 @app.route("/")
-async def test(request):
-    return text('Hello world!')
+async def index(request):
+    return html('<html>'
+                '   <body>'
+                '       <h2>Welcome to Sanic-CMS!</h2><br>'
+                '       settings:'
+                '       <pre>'
+                '           <span>DB_ENGINE: ' + app.config.DB_ENGINE + '</span><br>'
+                '           <span>DB_NAME: ' + app.config.DB_NAME + '</span><br>'
+                '       </pre>'
+                '   </body>'
+                '</html>')
 
 app.run(host="0.0.0.0", port=8000, debug=True)
