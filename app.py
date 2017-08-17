b893ea297843e0505da0b168f74e10eb96e9e24f
王琪胜 <wqs1639@gmail.com>
2017-08-17 13:59:30 +0800

    Adjust app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import os
 from sanic import Sanic
-from sanic.response import html
+from sanic.config import Config
+from sanic.response import html, text
+from sanic.exceptions import RequestTimeout
+
 from jinja2 import PackageLoader
 from sanic_jinja2 import SanicJinja2
+
 from config import DATABASE, THEMES
 from cms.models import db, User
+
 
 
 app = Sanic(__name__)
@@ -18,16 +23,38 @@ app.static('/static', './cms/static')
 template_package_loader = PackageLoader(app.name, 'cms/templates')
 template = SanicJinja2(app, loader=template_package_loader)
 
-# database create
-db_lock = 'db.lock'
-if not os.path.exists(db_lock):
-    db.create_tables([User])
-    open(db_lock,"w+").close()
+
+# Add listeners
+@app.listener('before_server_start')
+async def setup_db(app, loop):
+    # database create
+    db_lock = 'db.lock'
+    if not os.path.exists(db_lock):
+        db.create_tables([User])
+        open(db_lock, "w+").close()
+
+# Add middleware
 
 
-@app.route("/")
+@app.exception(RequestTimeout)
+def timeout(request, exception):
+    if request is not None:
+        print('ERROR HANDLE:\t', request, exception)
+        return text('RequestTimeout from error_handler.', 408)
+    return text('RequestTimeout from error_handler.', 408)
+
+
+# Add views
+
+
 async def index(request):
     """Index view"""
     return template.render('index.html', request, greetings='Welcome to Sanic-CMS')
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+# Add routes
+app.add_route(index, '/')
+app.add_route(index, '/index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
